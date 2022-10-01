1656555ac59c67be154d2bf3b18c27fa04ee3ec6
Karim.basher <kareem.basher@gmail.com>
2022-10-01 16:21:43 +0200

    Added more env variables.

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,17 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const { PORT, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env
+const {
+  PORT,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  BCRYPT_SECRET,
+  SALT,
+  SIGNITURE
+} = process.env
 
 export default {
   port: PORT,
@@ -11,5 +20,8 @@ export default {
   dbPort: POSTGRES_PORT,
   db: POSTGRES_DB,
   user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD
+  password: POSTGRES_PASSWORD,
+  pepper: BCRYPT_SECRET,
+  salt: SALT,
+  signiture: SIGNITURE
 }
