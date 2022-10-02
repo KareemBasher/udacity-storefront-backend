bfe2d54d0819065313f206b6e39a8630a94ab95d
kareembasher <kareem.basher@gmail.com>
2022-10-02 19:43:40 +0200

    Updated migrations.

--- migrations/sqls/20221001132400-users-table-up.sql
+++ migrations/sqls/20221001132400-users-table-up.sql
@@ -1,7 +1,5 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(50) NOT NULL,
-    user_name VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
--- migrations/sqls/20221001140511-products-table-up.sql
+++ migrations/sqls/20221001140511-products-table-up.sql
@@ -1,8 +1,6 @@
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL,
-    product_description VARCHAR(255) NOT NULL,
-    quantity INTEGER NOT NULL,
     price INTEGER NOT NULL,
     category VARCHAR(50) NOT NULL
 );
